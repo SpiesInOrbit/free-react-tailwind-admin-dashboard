7d99128ac5c1d8e6e26141307d1a5ea5d0bf19f3
Steve Lilley <SpiesInOrbit@gmail.com>
2024-06-25 00:56:00 -0500

    Icons and less clutter

--- src/pages/UiElements/Icons/index.ts
+++ src/pages/UiElements/Icons/index.ts
@@ -1,8 +1,10 @@
 
 import TempIcon from './TempIcon';
 import HumidityIcon from './HumidityIcon';
+import CameraIcon from './CameraIcon';
 
 export {
   TempIcon,
   HumidityIcon,
+  CameraIcon,
 };
