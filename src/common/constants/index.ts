55c1be969362126ec7517083bb97048ac8128306
Steve Lilley <SpiesInOrbit@gmail.com>
2024-08-28 13:00:48 -0500

    new constants

--- src/common/constants/index.ts
+++ src/common/constants/index.ts
@@ -1,9 +1,11 @@
 
 const url = import.meta.env?.[`VITE_${import.meta.env.MODE?.toUpperCase()}_API_URL`] || 'http://localhost';
+//const url = 'http://localhost';
 const port = null; //import.meta.env?.[`VITE_${import.meta.env.MODE?.toUpperCase()}_API_PORT`] || null;
+const version = 'v1';
 
 // api calls are re-rerouted to localhost:3000/api/ with nginx reverse proxy
-export const apiUrl = port === null ? `${url}/api` : `${url}:${port}/api`;
+export const apiUrl = port === null ? `${url}/api/${version}` : `${url}:${port}/api/${version}`;
 const defaultOn = "on"
 const defaultOff = "off"
 
